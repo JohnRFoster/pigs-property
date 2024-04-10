1824a44029b68b7502cdc45fbfea427aeb944d99
JohnRFoster <fosterj@bu.edu>
2024-04-10 10:08:45 -0600

    mostly comments, move plot dir to better spot

--- R/check_mcmc.R
+++ R/check_mcmc.R
@@ -15,27 +15,30 @@ library(ggplot2)
 
 source("R/functions_nimble.R")
 
-
-config_name <- "default"
+config_name <- "hpc_dev"
 config <- config::get(config = config_name)
 
 out_dir <- "/lustrefs/ceah/feral-swine/property-fits"
 np_dir <- paste0("dev", config$np)
-dest <- file.path(out_dir, np_dir)
 
+# where mcmc chunks are stored
+dest <- file.path(out_dir, np_dir)
 mcmc_dirs <- list.files(dest)
 param_file_name <- "paramSamples.rds"
 
+# use the first mcmc chunk to initialize storage for each chain
 mcmc_rds <- file.path(dest, mcmc_dirs[1], param_file_name)
 rds <- read_rds(mcmc_rds)
 mcmc <- rds$params
 n_chains <- length(mcmc)
 store_mcmc <- list()
 
+# store each chain, will append below
 for(j in seq_len(n_chains)){
   store_mcmc[[j]] <- as.matrix(mcmc[[j]])
 }
 
+# read each mcmc chunk, store each chain from the chunk as a matrix
 pb <- txtProgressBar(min = 2, max = length(mcmc_dirs), style = 1)
 for(i in 2:length(mcmc_dirs)){
   mcmc_rds <- file.path(dest, mcmc_dirs[i], param_file_name)
@@ -48,13 +51,14 @@ for(i in 2:length(mcmc_dirs)){
 }
 close(pb)
 
+# need to create an mcmc list object to check for convergence
 params_mcmc_list <- as.mcmc.list(lapply(store_mcmc, as.mcmc))
+
+# calculate psrf (convergence stat) and effective sample size
 diagnostic <- continue_mcmc(params_mcmc_list, effective_size = 5000, max_psrf = 15)
 
-np_dir <- paste0("dev", config$np, "combined")
-dest <- file.path(out_dir, np_dir)
-if(!dir.exists(dest)) dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-
+# X11 is not on the HPC, so need to use ggplot2 to create traceplots
+# which means we need to create a tibble of samples and identify chains and iterations
 posterior <- tibble()
 message("Create posterior tibble...")
 pb <- txtProgressBar(min = 1, max = n_chains, style = 1)
@@ -69,7 +73,7 @@ for(i in seq_len(n_chains)){
 close(pb)
 message("  done")
 
-
+# function to create traceplots from thinned posterior
 trace_plot <- function(post, nodes_2_plot, thin = 5000){
   df <- post |>
     select(chain, all_of(nodes_2_plot)) |>
@@ -97,13 +101,16 @@ trace_plot <- function(post, nodes_2_plot, thin = 5000){
 
 nodes <- setdiff(colnames(posterior), "chain")
 
+# want to put 4 nodes on a single plot
 plots <- tibble(
   nodes = nodes,
   idx = rep(seq(1, ceiling(length(nodes)/4), by = 1), each = 4)[1:length(nodes)]
 )
 
+# create dir for traceplots
 np_dir <- paste0("dev", config$np, "combined")
 dest <- file.path(out_dir, np_dir)
+if(!dir.exists(dest)) dir.create(dest, recursive = TRUE, showWarnings = FALSE)
 
 message("Creating traceplots...")
 pb <- txtProgressBar(min = 1, max = max(plots$idx), style = 1)
