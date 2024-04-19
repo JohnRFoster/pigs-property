218564d42658d575c9de8090f4a8e7b630928803
JohnRFoster <fosterj@bu.edu>
2024-04-19 16:25:04 -0600

    testing beta1

--- R/mcmc_configs.R
+++ R/mcmc_configs.R
@@ -5,6 +5,28 @@ nimbleMCMCdefs <- list(
     mcmcConf
   },
 
+  slice1 = function(model){
+    mcmcConf <- configureMCMC(model)
+    mcmcConf$removeSamplers(c("psi_phi", "log_nu"))
+    mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
+    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+    mcmcConf
+  },
+
+  beta1_AFslice = function(model){
+    mcmcConf <- configureMCMC(model)
+    mcmcConf$removeSamplers(c("beta1"))
+    mcmcConf$addSampler(target = c("beta1"), type = "AF_slice")
+    mcmcConf
+  },
+
+  beta1_RWblock = function(model){
+    mcmcConf <- configureMCMC(model)
+    mcmcConf$removeSamplers(c("beta1"))
+    mcmcConf$addSampler(target = c("beta1"), type = "RW_block")
+    mcmcConf
+  }
+
   # default_thetaBlock_afSlice = function(model){
   #   mcmcConf <- configureMCMC(model)
   #   for(i in 1:5){
@@ -31,109 +53,102 @@ nimbleMCMCdefs <- list(
   #   mcmcConf
   # },
 
-  slice0 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("log_nu"))
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf
-  },
-
-  slice1 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("psi_phi", "log_nu"))
-    mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf
-  },
-
-  slice2 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("phi_mu", "log_nu"))
-    mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf
-  },
-
-  slice3 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("beta_p", "log_nu"))
-    mcmcConf$addSampler(target = c("beta_p"), type = "AF_slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf
-  },
-
-  slice4 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu"))
-    mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
-    mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf
-  },
-
-  slice5 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "beta1"))
-    mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
-    mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf$addSampler(target = c("beta1"), type = "AF_slice")
-    mcmcConf
-  },
-
-  slice6 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "log_rho"))
-    mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
-    mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf$addSampler(target = c("log_rho"), type = "AF_slice")
-    mcmcConf
-  },
-
-  slice7 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "log_gamma"))
-    mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
-    mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf$addSampler(target = c("log_gamma"), type = "AF_slice")
-    mcmcConf
-  },
-
-  slice8 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "p_mu"))
-    mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
-    mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf$addSampler(target = c("p_mu"), type = "AF_slice")
-    mcmcConf
-  },
-
-  slice9 = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "beta_p"))
-    mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
-    mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf$addSampler(target = c("beta_p"), type = "AF_slice")
-    mcmcConf
-  },
-
-  sliceALL = function(model){
-    mcmcConf <- configureMCMC(model)
-    mcmcConf$removeSamplers(c("beta_p", "beta1", "log_gamma", "p_mu", "log_rho", "phi_mu", "psi_phi", "log_nu"))
-    mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
-    mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
-    mcmcConf$addSampler(target = c("log_nu"), type = "slice")
-    mcmcConf$addSampler(target = c("log_gamma"), type = "AF_slice")
-    mcmcConf$addSampler(target = c("log_rho"), type = "AF_slice")
-    mcmcConf$addSampler(target = c("p_mu"), type = "AF_slice")
-    mcmcConf$addSampler(target = c("beta_p"), type = "AF_slice")
-    mcmcConf$addSampler(target = c("beta1"), type = "AF_slice")
-    mcmcConf
-  }
+  # slice0 = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("log_nu"))
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf
+  # },
+  #
+  #
+  # slice2 = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("phi_mu", "log_nu"))
+  #   mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf
+  # },
+  #
+  # slice3 = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("beta_p", "log_nu"))
+  #   mcmcConf$addSampler(target = c("beta_p"), type = "AF_slice")
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf
+  # },
+  #
+  # slice4 = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu"))
+  #   mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf
+  # },
+  #
+  # slice5 = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "beta1"))
+  #   mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("beta1"), type = "AF_slice")
+  #   mcmcConf
+  # },
+  #
+  # slice6 = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "log_rho"))
+  #   mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_rho"), type = "AF_slice")
+  #   mcmcConf
+  # },
+  #
+  # slice7 = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "log_gamma"))
+  #   mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_gamma"), type = "AF_slice")
+  #   mcmcConf
+  # },
+  #
+  # slice8 = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "p_mu"))
+  #   mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("p_mu"), type = "AF_slice")
+  #   mcmcConf
+  # },
+  #
+  # slice9 = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("phi_mu", "psi_phi", "log_nu", "beta_p"))
+  #   mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("beta_p"), type = "AF_slice")
+  #   mcmcConf
+  # },
+  #
+  # sliceALL = function(model){
+  #   mcmcConf <- configureMCMC(model)
+  #   mcmcConf$removeSamplers(c("beta_p", "beta1", "log_gamma", "p_mu", "log_rho", "phi_mu", "psi_phi", "log_nu"))
+  #   mcmcConf$addSampler(target = c("phi_mu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("psi_phi"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_nu"), type = "slice")
+  #   mcmcConf$addSampler(target = c("log_gamma"), type = "AF_slice")
+  #   mcmcConf$addSampler(target = c("log_rho"), type = "AF_slice")
+  #   mcmcConf$addSampler(target = c("p_mu"), type = "AF_slice")
+  #   mcmcConf$addSampler(target = c("beta_p"), type = "AF_slice")
+  #   mcmcConf$addSampler(target = c("beta1"), type = "AF_slice")
+  #   mcmcConf
+  # }
 
   # slice_scalars2 = function(model){
   #   mcmcConf <- configureMCMC(model)
