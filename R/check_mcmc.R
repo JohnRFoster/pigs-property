8cc41e6577d7b49e47f742c4a617c2a91e935000
JohnRFoster <fosterj@bu.edu>
2024-04-24 14:23:48 -0400

    xlib again, actually pull on hpc

--- R/check_mcmc.R
+++ R/check_mcmc.R
@@ -129,6 +129,7 @@ plots <- tibble(
 )
 
 message("Creating traceplots...")
+options(bitmapType = 'Xlib')
 pb <- txtProgressBar(min = 1, max = max(plots$idx), style = 1)
 for(i in seq_along(unique(plots$idx))){
   n2p <- plots |>
@@ -137,7 +138,7 @@ for(i in seq_along(unique(plots$idx))){
 
   gg <- trace_plot(posterior, n2p)
 
-  filename <- file.path(dest, paste0("mcmcTimeseries_", sprintf("%03d", i), ".png"))
+  filename <- file.path(dest, paste0("mcmcTimeseries_", sprintf("%03d", i), ".jpeg"))
   ggsave(filename, gg)
 
   setTxtProgressBar(pb, i)
