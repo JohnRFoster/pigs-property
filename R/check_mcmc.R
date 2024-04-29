4d9bf4335c7f0f5147dd74f44204d43c0c58e62e
JohnRFoster <fosterj@bu.edu>
2024-04-29 15:03:15 -0600

    check 42

--- R/check_mcmc.R
+++ R/check_mcmc.R
@@ -19,7 +19,7 @@ config_name <- "hpc_dev"
 config <- config::get(config = config_name)
 
 out_dir <- "/lustrefs/ceah/feral-swine/property-fits"
-np <- 22
+np <- 42
 np_dir <- file.path("dev", paste0(np, "_properties"))
 
 # where mcmc chunks are stored
