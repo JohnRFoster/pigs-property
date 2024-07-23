2b78a421e3d58f7eed5f6bc4ef87bb02a6dc0c4f
JohnRFoster <fosterj@bu.edu>
2024-07-23 13:41:52 -0600

    better inits for 77 fit

--- R/functions_prep_nimble.R
+++ R/functions_prep_nimble.R
@@ -384,12 +384,18 @@ nimble_inits <- function(constants_nimble, data_nimble, buffer = 1000){
 
   with(append(constants_nimble, data_nimble), {
 
-    beta1 <- jitter(c(-2, 0, 0.5, -3.5, -3.5))
-    beta_p <- matrix(jitter(c(2.5, -2, -0.5, -1, -0.5, 0, 0, 1, -1.5, -0.5, 1, 1, 0.5, 0, 0.25)), n_method, m_p)
-    p_mu <- jitter(c(-2.5, -3))
-    log_gamma <- jitter(c(-2, -1.5))
-    log_rho <- jitter(c(2.4, 2, 5, 0.4, 0.8))
-    psi_phi <- runif(1, 3, 5)
+    beta1 <- jitter(c(-1, 0.5, 1.5, -2, 0))
+    beta_p <- matrix(jitter(
+      c(2,  1,   1,
+        -1,  0, 1,
+        2,    -2,    0,
+        -4, -2,    1,
+        -0.5,  2.5,    0)),
+      n_method, m_p)
+    p_mu <- jitter(c(-3, -3))
+    log_gamma <- jitter(c(-3, -1.5))
+    log_rho <- jitter(c(2, 2, 1.5, -0.5, 0))
+    psi_phi <- runif(1, 0.6, 1)
     phi_mu <- runif(1, 0.7, 0.8)
     mean_ls <- round(runif(1, 11, 12))
 
