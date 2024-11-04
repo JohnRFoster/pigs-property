05f7c6d9969d01c63aef3438194b32795e2b4065
JohnRFoster <john.foster@usda.gov>
2024-11-04 09:29:28 -0600

    rm max runtime

--- slurm_mcmc.sh
+++ slurm_mcmc.sh
@@ -2,10 +2,11 @@
 #SBATCH --ntasks=1                      # number of processes to run
 #SBATCH --nodes=1                       # default number of nodes
 #SBATCH --partition=cpu_compute         # good enough for what I need
-#SBATCH --cpus-per-task=2               # for a multithredded job
-#SBATCH --mem=24g                       # memory
-#SBATCH --job-name=check_300                 # job name
-#SBATCH --output=outfiles/check_300.txt      # output file
+#SBATCH --cpus-per-task=7               # for a multithredded job
+#SBATCH --mem=96g                       # memory
+# #SBATCH --time=144:00:00
+#SBATCH --job-name=iteratuveBatch             # job name
+#SBATCH --output=outfiles/iterativeBatch.txt    # output file
 
 module add R
-Rscript R/check_mcmc.R
+Rscript 1_workflow_mcmc.R
