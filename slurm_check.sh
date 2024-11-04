a87653e307abdb53399bd6482d25d838dbffeaca
JohnRFoster <john.foster@usda.gov>
2024-11-04 09:29:56 -0600

    check batch

--- slurm_check.sh
+++ slurm_check.sh
@@ -4,8 +4,8 @@
 #SBATCH --partition=cpu_compute         # good enough for what I need
 #SBATCH --cpus-per-task=2               # for a multithredded job
 #SBATCH --mem=24g                       # memory
-#SBATCH --job-name=check_300                 # job name
-#SBATCH --output=outfiles/check_300.txt      # output file
+#SBATCH --job-name=checkFirst                 # job name
+#SBATCH --output=outfiles/checkFirst.txt      # output file
 
 module add R
 Rscript R/check_mcmc.R
